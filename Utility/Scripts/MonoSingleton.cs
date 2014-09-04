6c2afb2e116497ea71785555fa46146e94c71ab4
almyu <hire@indeed.li>
2014-09-05 01:01:05 +0800

    Add MonoSingleton

---
+++ Utility/Scripts/MonoSingleton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace UnityEngine {
+
+    public class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour {
+
+        public static T instance {
+            get {
+                if (!_instance)
+                    _instance = FindObjectOfType<T>();
+
+                return _instance;
+            }
+        }
+
+        protected static T _instance;
+    }
+}
