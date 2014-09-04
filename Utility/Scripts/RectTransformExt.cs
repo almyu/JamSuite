3fc5b1044ef7990f5b316800f29071bb51f83277
almyu <hire@indeed.li>
2014-09-05 02:02:53 +0800

    Add RectTransformExt

---
+++ Utility/Scripts/RectTransformExt.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace UnityEngine {
+
+    public static class RectTransformExt {
+
+        public static void AddChild(this RectTransform xf, RectTransform other) {
+            var initialPosition = other.anchoredPosition;
+            var initialSize = other.sizeDelta;
+
+            other.parent = xf;
+
+            other.anchoredPosition = initialPosition;
+            other.sizeDelta = initialSize;
+        }
+    }
+}
