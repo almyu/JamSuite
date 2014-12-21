a4526fc6fde5587b7695c15cfcd26b090454fee6
almyu <hire@indeed.li>
2014-12-22 01:00:24 +0700

    Add TimescaleStack

---
+++ Utility/Scripts/TimescaleStack.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public static class TimescaleStack {
+
+    private static Stack<float> stack = new Stack<float>();
+
+    public static void Push(float scale) {
+        stack.Push(Time.timeScale);
+        Time.timeScale = scale;
+    }
+
+    public static float Pop() {
+        var last = Time.timeScale;
+        Time.timeScale = stack.Pop();
+        return last;
+    }
+}
