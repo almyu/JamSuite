2380d24066cf5cb9392dbfb84f50897829e8a3ab
almyu <hire@indeed.li>
2014-12-23 09:12:47 +0700

    Update InterstageFade

--- Utility/Scripts/InterstageFade.cs
+++ Utility/Scripts/InterstageFade.cs
@@ -4,7 +4,8 @@ using System.Collections;
 [RequireComponent(typeof(SpriteRenderer))]
 public class InterstageFade : MonoBehaviour {
 
-    public AnimationCurve inTransition, outTransition;
+    public AnimationCurve inTransition = AnimationCurve.EaseInOut(0f, 0f, 0.5f, 1f);
+    public AnimationCurve outTransition = AnimationCurve.EaseInOut(0f, 1f, 0.5f, 0f);
 
     public void FadeToLevel(string level) {
         StartCoroutine(DoFade(level));
@@ -24,7 +25,7 @@ public class InterstageFade : MonoBehaviour {
         var renderer = GetComponent<SpriteRenderer>();
         var duration = curve[curve.length - 1].time;
 
-        for (var t = 0f; t < duration; t += Mathf.Max(0.001f, Time.deltaTime)) {
+        for (var t = 0f; t <= duration; t += Mathf.Max(0.001f, Time.deltaTime)) {
             renderer.color = renderer.color.WithA(curve.Evaluate(t));
             yield return null;
         }
