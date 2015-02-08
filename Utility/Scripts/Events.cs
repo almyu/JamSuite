b1c47be6bdcc121346bf9fc6860fef79a3f693e9
almyu <hire@indeed.li>
2015-02-08 21:05:18 +0700

    Add Events

---
+++ Utility/Scripts/Events.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System;
+
+namespace JamSuite.Events {
+
+    [Serializable] public class IntEvent : UnityEvent<int> {}
+    [Serializable] public class FloatEvent : UnityEvent<float> {}
+    [Serializable] public class Vector2Event : UnityEvent<Vector2> {}
+    [Serializable] public class Vector3Event : UnityEvent<Vector3> {}
+    [Serializable] public class Vector4Event : UnityEvent<Vector4> {}
+    [Serializable] public class RectEvent : UnityEvent<Rect> {}
+    [Serializable] public class StringEvent : UnityEvent<string> {}
+}
