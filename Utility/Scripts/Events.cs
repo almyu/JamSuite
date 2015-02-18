33ba8189d9aa35dfd0c476436879cf8fa138bfd7
almyu <hire@indeed.li>
2015-02-18 21:27:01 +0700

    Add BoolEvent

--- Utility/Scripts/Events.cs
+++ Utility/Scripts/Events.cs
@@ -4,6 +4,7 @@ using System;
 
 namespace JamSuite.Events {
 
+    [Serializable] public class BoolEvent : UnityEvent<bool> {}
     [Serializable] public class IntEvent : UnityEvent<int> {}
     [Serializable] public class FloatEvent : UnityEvent<float> {}
     [Serializable] public class Vector2Event : UnityEvent<Vector2> {}
