57fb0aab1b6503f865a8b02fd45bd108d0b18d82
Anton Tarasov <uchiro.me@gmail.com>
2015-02-21 21:15:49 +0600

    Add EventButton

---
+++ Utility/Scripts/EventButton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class EventButton : MonoBehaviour {
+
+    public string buttonName;
+
+    public UnityEvent getButton;
+    public UnityEvent getButtonDown;
+    public UnityEvent getButtonUp;
+    
+    private void Update () {
+        if (Input.GetButton(buttonName)) getButton.Invoke();
+        if (Input.GetButtonUp(buttonName)) getButtonUp.Invoke();
+        if (Input.GetButtonDown(buttonName)) getButtonDown.Invoke();
+    }
+}
