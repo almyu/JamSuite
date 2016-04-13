100ad2f4bf8b3193c54fa18308779ee9cc476482
almyu <hire@indeed.li>
2016-04-13 19:43:13 +0700

    Replace useless crap with somewhat less useless crap :recycle:

---
+++ Persistent/AutoSpawn.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace JamSuite.Persistent {
+
+    public static class AutoSpawn {
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        public static void SpawnAll() {
+            foreach (var prefab in Resources.LoadAll<GameObject>("AutoSpawn"))
+                GameObject.DontDestroyOnLoad(GameObject.Instantiate(prefab));
+        }
+    }
+}
