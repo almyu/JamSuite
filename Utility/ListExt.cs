2dd411d7bd8197c42fafecb6d8a140e8d6e8fa58
almyu <hire@indeed.li>
2016-05-04 01:25:53 +0700

    Update ListExt

--- Utility/ListExt.cs
+++ Utility/ListExt.cs
@@ -17,6 +17,19 @@ public static class ListExt {
         }
     }
 
+    public static void SwapRemoveAt<T>(this IList<T> list, int index) {
+        list[index] = list[list.Count - 1];
+        list.RemoveAt(list.Count - 1);
+    }
+
+    public static bool SwapRemove<T>(this IList<T> list, T item) {
+        var index = list.IndexOf(item);
+        if (index == -1) return false;
+
+        list.SwapRemoveAt(index);
+        return true;
+    }
+
     public static string ToString<T>(this IEnumerable<T> seq, string separator, string prefix = "", string suffix = "") {
         var str = new StringBuilder(prefix);
         var first = true;
