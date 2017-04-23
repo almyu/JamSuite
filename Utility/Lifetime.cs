f4af1efd6b0d81c6978636d2884d77dcaaf5570b
almyu <hire@indeed.li>
2017-04-24 01:08:05 +0700

    Add Lifetime

---
+++ Utility/Lifetime.cs
@@ -0,0 +1,43 @@
+﻿/// Version: 2017-03-30
+
+using UnityEngine;
+
+namespace JamSuite
+{
+    public sealed class Lifetime : MonoBehaviour
+    {
+        public float lifetime = 1f;
+
+        public void InferFromParticles() {
+            lifetime = 1f;
+
+            foreach (var ps in GetComponentsInChildren<ParticleSystem>()) {
+                var time = ps.main.duration + ps.main.startLifetime.constantMax;
+                if (lifetime < time) lifetime = time;
+            }
+        }
+
+        void Reset() {
+            InferFromParticles();
+        }
+
+        void Start() {
+            Destroy(gameObject, lifetime);
+        }
+    }
+
+#if UNITY_EDITOR
+    [UnityEditor.CustomEditor(typeof(Lifetime))]
+    [UnityEditor.CanEditMultipleObjects]
+    public class LifetimeInspector : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI() {
+            DrawDefaultInspector();
+
+            if (GUILayout.Button("Infer From Particles"))
+                foreach (Lifetime target in targets)
+                    target.InferFromParticles();
+        }
+    }
+#endif
+}
